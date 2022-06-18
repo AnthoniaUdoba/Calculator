d6af8f8e132f6be99b56906b96c1b4dd98eacbe8
Anthonia Udoba <99968773+AnthoniaUdoba@users.noreply.github.com>
2022-06-18 18:09:47 +0100

    Add files via upload

--- CalcScript.js
+++ CalcScript.js
@@ -3,9 +3,68 @@ const operatorsBtn = document.querySelectorAll(".operator");
 
 const equaltoBtn = document.querySelector(".equal-to");
 const clearBtn = document.querySelector(".clear");
-const deleteLastBtn = document.querySelectorAll(".delete");
-const plusMinusBtn = document.querySelectorAll(".plus-minus");
+const deleteLastBtn = document.querySelector(".delete");
+const plusMinusBtn = document.querySelector(".plus-minus");
 const dotBtn = document.querySelector(".dot");
-
 let display = document.querySelector(".display");
 let output = document.querySelector(".output");
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+numbersBtn.forEach((number) => {
+  number.addEventListener(
+    "click",
+    () => (display.innerText += number.innerText)
+  );
+});
+
+operatorsBtn.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (num1 !== "") return;
+    if (num2 !== "") {
+      mathOperation(operator, num1, num2);
+    }
+    display.innerText = operator.innerText;
+  });
+});
+
+function mathOperation(operator, num1, num2) {
+  if (operator.innerText === "×") {
+    operator = parseFloat(num1) * parseFloat(num2);
+  } else if (operator.innerText === "-") {
+    operator = parseFloat(num1) - parseFloat(num2);
+  } else if (operator.innerText === "+") {
+    operator = parseFloat(num1) + parseFloat(num2);
+  } else if (operator.innerText === "÷") {
+    operator = parseFloat(num1) / parseFloat(num2);
+  } else if (operator.innerText === "%") {
+    operator = parseFloat(num1) % parseFloat(num2);
+  }
+}
+
+equaltoBtn.addEventListener("click", () => {
+  if (output.innerText !== "") {
+    display.innerText = output.innerText;
+    output.innerHTML = "";
+  }
+});
+
+// clearBtn.addEventListener("click", () => {
+//   equalPressed = 0;
+//   displayBtn.textContent = "";
+//   outputBtn.textContent = "";
+// });
+
+// deleteLastBtn.addEventListener("click", () => {
+//   displayBtn.textContent = displayBtn.textContent.toString().slice(0, -1);
+//   display = "";
+// });
+
+// dotBtn.addEventListener("click", function () {
+//   if (equalPressed) {
+//     displayBtn.textContent = "";
+//     equalPressed = false;
+//   }
+// });
